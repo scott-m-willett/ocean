567e11850fe26a81c4f8032435f461198d364ce7
Scott Willett <swillett@protonmail.com>
2021-04-03 23:06:28 +1000

    Update version to contain a dependency, and add basic info to README

---
+++ README.md
@@ -0,0 +1,7 @@
+
+Pentesting automations with the Digital Ocean Cloud Platform and Droplets (VMs).
+
+https://pypi.org/project/ocean-pentest/0.3/
+
+
+pip3 install ocean-pentest
--- setup.py
+++ setup.py
@@ -6,19 +6,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
  
 setuptools.setup(
-    name='ocean',  
-    version='0.2',
+    name='ocean-pentest',  
+    version='0.3',
     author="Scott Willett",
     author_email="swillett@protonmail.com",
     description="Automates many tasks for pentesting with the Digital Ocean Cloud Platform.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/scott-m-willett/Ocean",
+    url="https://github.com/scott-m-willett/ocean",
+    download_url="https://github.com/scott-m-willett/ocean/archive/refs/tags/v0.2-alpha.tar.gz",
     packages=["ocean","ocean.classes"],
     entry_points = {
         "console_scripts": ['ocean = ocean:main']
     },
-    install_requires=[],
+    install_requires=['python-digitalocean','paramiko'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
